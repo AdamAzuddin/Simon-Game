07f1424672793fb7e63b109a195bc4c1a857b5ac
AdamAzuddin <05adamaz@gmail.com>
2023-04-28 10:46:20 +0800

    press any key to start

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const buttonNum = {
-    "green": 0,
-    "red":1,
-    "yellow":2,
-    "blue":3
+
+
+function start() {
+  $("h1").text("Press Any Key to Start");
+  $(document).keydown(function () {
+    startLevel(1);
+  });
 }
 
-function addRandomNumToAns() {
-    var answer = [];
-    var randomButton = Math.random() * 4;
-    randomButton = Math.round(randomButton);
-    answer.push(randomButton);
-    return answer
-  }
-
+start();
