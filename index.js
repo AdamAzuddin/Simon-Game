669fe1ca7467f8d2ce9ab6094a9840da9fc2212f
AdamAzuddin <05adamaz@gmail.com>
2023-04-28 21:58:42 +0800

    donee

--- index.js
+++ index.js
@@ -1,12 +1,108 @@
-function start() {
-  $("h1").text("Press Any Key to Start");
-  $(document).keydown(function () {
-    startLevel(3);
+var level = 1;
+var userArray = [];
+var currentPoint = 0;
+$("h1").text("Press Any Key to Start");
+$(document).one("keydown", function () {
+  $("h1").text(`Starting level ${level}`);
+  var correctArray = addRandomNumToAns(level);
+  console.log(`Correct answer: ` + correctArray);
+  $(".btn").click(function (e) {
+    e.preventDefault();
+    // convert button id to array index num
+    var btnNum = idToNum($(this).attr("id"));
+    
+    userArray.push(btnNum);
+    console.log("Your answer: " + userArray);
+    for (let index = 0; index < userArray.length; index++) {
+      if (userArray[index] !== correctArray[index]) {
+        console.log("Wrong!");
+        $("h1").text("Game Over!");
+        var isWin = false;
+        break;
+      }
+      if (userArray[index] == correctArray[index]) {
+        currentPoint = userArray.length;
+
+        console.log(currentPoint + "/" + correctArray.length);
+        if (currentPoint == correctArray.length) {
+          var isWin = true;
+          console.log("You Win");
+          break;
+        }
+      }
+    }
+    if (isWin) {
+      level++;
+      console.log(level);
+      userArray = [];
+      currentPoint = 0;
+      correctArray=[]
+
+      $(".btn").off("click");
+      startLevel(level);
+    }
+  });
+});
+
+function startLevel(level) {
+  $("h1").text(`Starting level ${level}`);
+  var correctArray = addRandomNumToAns(level);
+  console.log(`Correct answer: ` + correctArray);
+  $(".btn").click(function (e) {
+    e.preventDefault();
+    console.log("Hi");
+    // TODO: Complete this function
+    var btnNum = idToNum($(this).attr("id"));
+    userArray.push(btnNum);
+    console.log("Your answer: " + userArray);
+    for (let index = 0; index < userArray.length; index++) {
+      if (userArray[index] !== correctArray[index]) {
+        console.log("Wrong!");
+        $("h1").text("Game Over!");
+        var isWin = false;
+        break;
+      }
+      if (userArray[index] == correctArray[index]) {
+        currentPoint = userArray.length;
+
+        console.log(currentPoint + "/" + correctArray.length);
+        if (currentPoint == correctArray.length) {
+          var isWin = true;
+          console.log("You Win");
+          break;
+        }
+      }
+    }
+    if (isWin) {
+      level++;
+      console.log(level);
+      userArray = [];
+      currentPoint = 0;
+
+      $(".btn").off("click");
+      startLevel(level);
+    }
   });
 }
 
-function startLevel(level) {
-  addRandomNumToAns(level);
+function idToNum(id) {
+  switch (id) {
+    case "green":
+      var btnNum = 0;
+      break;
+    case "red":
+      var btnNum = 1;
+      break;
+    case "yellow":
+      var btnNum = 2;
+      break;
+    case "blue":
+      var btnNum = 3;
+      break;
+    default:
+      break;
+  }
+  return btnNum;
 }
 function addRandomNumToAns(length) {
   var answer = [];
@@ -26,11 +122,6 @@ function addRandomNumToAns(length) {
   }
 
   return answer;
-}
-
-function showAns(answer) {
-  // change sound by adding class
-  pressButton(answer);
 }
 
 function pressButton(number) {
@@ -62,8 +153,6 @@ function pressButton(number) {
 
 function playSound(fileName) {
   var audio = new Audio(`./sounds/${fileName}.mp3`);
-  audio.play();
-  return;
+  audio = audio.play();
+  return audio;
 }
-
-start();
